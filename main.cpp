aa9200578d60c81ed917f264bd83e57a40c436e5
saadmehsud313 <mehsudsaad313@gmail.com>
2025-06-05 15:03:15 +0500

    Enhance role-based login functionality and improve user experience in the Bank Management System

--- main.cpp
+++ main.cpp
@@ -1,57 +1,161 @@
 #include <iostream>
 #include <iomanip>
-
+#include <string>
 using namespace std;
 
-bool validdateCredentials(string usename, string password,string pos)
+bool validateCredentials(string username, string password,string pos)
 {
+    cout<<"Position:"<<pos<<endl;
     string usn="Saad",pass="1234";
 
-    if(usename==usn && password==pass)
+    if(pos== "admin")
     {
-        cout<<"Login Successful"<<endl;
-        return true;
+        cout<<"Admin Selected"<<endl;
+
     }
-    else
+    else if(pos == "staff")
     {
-        cout<<"Invalid Credentials"<<endl;
-        return false;
+        cout<<"Staff Selected"<<endl;
     }
+    else if(pos == "atm")
+    {
+        cout<<"ATM Selected"<<endl;
+    }
+    else{
+        cout<<"Position Not  Recognized"<<endl;
+    }
+    return true;
 }
 
 int main()
 {
+    char ch='*';
     int choice=1,choice1=1,cnt=3;
     string username, password;
     cout<<"**********************************wELCOME TO BANK MANAGEMENT SYSTEM**********************************"<<endl;
-    cout<<"********************************************************************************************************** \n\n\n"<<endl;
+    cout<<setfill(ch)<<setw(120)<<"*"<<endl;
     while(choice!=0)
     {
-        cout<<setfill('*')<<setw(60)<<"    Please Select Your Role     "<<setfill('*')<<setw(50)<<'*'<<endl;
-        cout<<setfill(' ')<<setw(50)<<"1.For Admin      "<<endl;
-        cout<<setfill(' ')<<setw(50)<<"2. For Staff     "<<endl;
-        cout<<setfill(' ')<<setw(57)<<"3. For ATM Services     "<<endl;
-        cout<<setfill(' ')<<setw(51)<<"  0.For Exit        "<<endl;
-        cout<<setfill(' ')<<setw(52)<<"Enter your choice: ";
+        cout<<"    Please Select Your Role     "<<endl;
+        cout<<"    1.For Admin      "<<endl;
+        cout<<"    2. For Staff     "<<endl;
+        cout<<"    3. For ATM Services     "<<endl;
+        cout<<"     0.For Exit        "<<endl;
+        cout<<"Enter your choice: ";
         cin>>choice;
         
         switch(choice)
         {
             case 1:
                 do{
-                cout<<setfill(' ')<<setw(50)<<"Remaining Attempts: "<<cnt<<endl;
-                cout<<setfill(' ')<<setw(50)<<"Please Insert Your Credentials:"<<endl;
-                cout<<setfill(' ')<<setw(50)<<"Username: ";
+                cout<<"Remaining Attempts: "<<cnt<<endl;
+                cout<<"Please Insert Your Credentials:"<<endl;
+                cout<<"Username: ";
                 cin>>username;
-                cout<<setfill(' ')<<setw(50)<<"Password: ";
+                cout<<"Password: ";
                 cin>>password;
                 cnt--;
-                } while(!validdateCredentials(username, password,"admin") && cnt != 0);
+                cnt == 0 ? cout<<"You have exhausted all attempts. Exiting..."<<endl : cout<<endl; 
+            } while(!validateCredentials(username, password,"admin") && cnt != 0);
+                while(choice1!=0)
+                {
+                    cout<<"Welcome "<<username<<",you are logged in as Admin."<<endl;
+                    cout<<"Please select an operation you want to perform :"<<endl;
+                    cout<<"Press 1 to Create new account."<<endl;
+                    cout<<"Press 2 to Delete existing account."<<endl;
+                    cout<<"Press 3 to search for an account."<<endl;
+                    cout<<"Press 4 to View all accounts."<<endl;
+                    cout<<"Press 5 to Update account details."<<endl;
+                    cout<<"Press 6 to view logs of deleted accounts."<<endl;
+                    cout<<"Press 0 to Logout."<<endl;
+                    cout<<"Enter your choice: ";
+                    cin>>choice1;
+                    switch(choice1)
+                    {
+                        case 1:
+                            cout<<"Creating new account..."<<endl;
+                            // Add account creation functionality here
+                            break;
+                        case 2:
+                            cout<<"Deleting existing account..."<<endl;
+                            // Add account deletion functionality here
+                            break;
+                        case 3:
+                            cout<<"Searching for an account..."<<endl;
+                            // Add account search functionality here
+                            break;
+                        case 4:
+                            cout<<"Viewing all accounts..."<<endl;
+                            // Add view all accounts functionality here
+                            break;
+                        case 5:
+                            cout<<"Updating account details..."<<endl;
+                            // Add update account details functionality here
+                            break;
+                        case 6:
+                            cout<<"Viewing logs of deleted accounts..."<<endl;
+                            // Add view logs funtionality here 
+                            break;
+                        case 0:
+                            cout<<"Logging out..."<<endl;
+                            choice1 = 0; // Exit the admin menu
+                            break;
+                        default:
+                            cout<<"Invalid choice. Please try again."<<endl;
+                    }
+                }
                 
                 break;
             case 2:
                 cout<<"You selected Staff."<<endl;
+                do{
+                cout<<"Remaining Attempts: "<<cnt<<endl;
+                cout<<"Please Insert Your Credentials:"<<endl;
+                cout<<"Username: ";
+                cin>>username;
+                cout<<"Password: ";
+                cin>>password;
+                cnt--;
+                cnt == 0 ? cout<<"You have exhausted all attempts. Exiting..."<<endl : cout<<endl ;
+                } while(!validateCredentials(username, password,"staff") && cnt != 0);
                 // Add staff functionality here
+                while(choice1!=0)
+                {
+                    cout<<"Welcome "<<username<<",you are logged in as Admin."<<endl;
+                    cout<<"Please select an operation you want to perform :"<<endl;
+                    cout<<"Press 1 to Check Account."<<endl;
+                    cout<<"Press 2 to Withdraw cash."<<endl;
+                    cout<<"Press 3 to deposit cash."<<endl;
+                    cout<<"Press 4 to view logs of selected account."<<endl;
+                    cout<<"Press 0 to Logout."<<endl;
+                    cout<<"Enter your choice: ";
+                    cin>>choice1;
+                    switch(choice1)
+                    {
+                        case 1:
+                            cout<<"Checking account..."<<endl;
+                            // Add check account functionality here
+                            break;
+                        case 2:
+                            cout<<"Withdrawing cash..."<<endl;
+                            // Add withdraw cash functionality here
+                            break;
+                        case 3:
+                            cout<<"Depositing cash..."<<endl;
+                            // Add deposit cash functionality here
+                            break;
+                        case 4:
+                            cout<<"Viewing logs of selected account..."<<endl;
+                            // Add view logs functionality here
+                            break;
+                        case 0:
+                            cout<<"Logging out..."<<endl;
+                            choice1 = 0; // Exit the staff menu
+                            break;
+                        default:
+                            cout<<"Invalid choice. Please try again."<<endl;
+                    }
+                }
                 break;
             case 3:
                 cout<<"You selected ATM Services."<<endl;
